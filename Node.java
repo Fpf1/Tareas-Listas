4c2a3945d503ff443d168f3856a0d6b456f36b64
Fpf1 <115376402+Fpf1@users.noreply.github.com>
2023-11-27 23:28:13 -0500

    ListasSimples

--- Node.java
+++ Node.java
@@ -1,7 +1,8 @@
-//clase autorreferenciada
-
 public class Node {
-	String name; //campo de datos
-	Node previous; //campo enlace
-    Node next;   //campo enlace
+    String name; // campo de datos
+    Node next; // campo enlace
+
+    public String toString() {
+        return name;
+    }
 }
